c11cc370f1cf2ee1896f799bcd189c890e023fe7
Dasuntharuka34 <152604324+Dasuntharuka34@users.noreply.github.com>
2026-02-06 14:04:07 +0530

    feat: add a new Contact Support page with contact information.

--- src/pages/ContactSupportPage.jsx
+++ src/pages/ContactSupportPage.jsx
@@ -7,7 +7,7 @@ function ContactSupportPage() {
       <h1>Contact Support</h1>
       <p>If you have any issues or questions, please contact us through one of the following methods:</p>
       <ul>
-        <li><strong>Email:</strong> FindMyDocsTeam@gmail.com</li>
+        <li><strong>Email:</strong> findmydocsteam00@gmail.com</li>
         <li><strong>Phone:</strong> +94 11 222 2222</li>
       </ul>
     </div>
